b649802e58aaba53fddb6e712b6d6cf9ec533345
Cameron Slipp <cdslipp@gmail.com>
2022-02-08 01:57:59 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,26 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+     <script src="https://cdn.jsdelivr.net/npm/tripetto-runner-foundation"></script>
+<script src="https://cdn.jsdelivr.net/npm/tripetto-runner-autoscroll"></script>
+<script src="https://cdn.jsdelivr.net/npm/tripetto-services"></script>
+<script>
+var tripetto = TripettoServices.init({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiRG9jTzF4WlIrNUJpMEgvOGh6RmNpNlR3UTRYMlJVYnU0d3dkbHMyMnBDTT0iLCJkZWZpbml0aW9uIjoiWUM1dU4yRXR5a3RPT0JIaUVoZEJvT0FpZXNoUmd5YWNZWVJVSisxbTQ2MD0iLCJ0eXBlIjoiY29sbGVjdCJ9.5MT1ZmnGxkQGLAVHhnrwoM_0jmPbu4f33JkokfaqKFU" });
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+TripettoAutoscroll.run({
+    element: document.body, /* Or supply your own element here */
+    definition: tripetto.definition,
+    styles: tripetto.styles,
+    l10n: tripetto.l10n,
+    locale: tripetto.locale,
+    translations: tripetto.translations,
+    attachments: tripetto.attachments,
+    onSubmit: tripetto.onSubmit,
+    snapshot: tripetto.snapshot,
+    onPause: tripetto.onPause,
+    persistent: true
+});
+</script>
     </div>
   )
 }
